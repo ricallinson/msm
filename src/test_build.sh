b06e1bbb3e274806f7db9699dbc1ca4a3e649773
Ric Allinson <ric@ricallinson.com>
2018-03-29 18:21:36 -0700

    Test for building

--- src/test_build.sh
+++ src/test_build.sh
@@ -14,11 +14,22 @@
 # shellcheck source=./src/msm.sh
 source "$TESTMSMHOME/src/msm.sh"
 
-data="$(msm env)"
-if [[ "$data" != *"MSMPATH"* && "$data" != *"MSMHOME"* ]]; then
-	echo "Environment not returned"
+WD=$(pwd)
+
+rm -rf "$WD/tmp"
+mkdir "$WD/tmp"
+cd "$WD/tmp" || exit 1
+
+msm here . > /dev/null 2>&1
+msm build > /dev/null 2>&1
+
+if [[ ! -f "./pkg/service.img" ]]; then
+	echo "Service disk image was not created."
 	exit 1
 fi
+
+cd "$WD" || exit 1
+rm -rf "$WD/tmp"
 
 exit 0
 }
--- src/test_here.sh
+++ src/test_here.sh
@@ -14,11 +14,35 @@
 # shellcheck source=./src/msm.sh
 source "$TESTMSMHOME/src/msm.sh"
 
-data="$(msm env)"
-if [[ "$data" != *"MSMPATH"* && "$data" != *"MSMHOME"* ]]; then
-	echo "Environment not returned"
+WD=$(pwd)
+
+rm -rf "$WD/tmp"
+mkdir "$WD/tmp"
+cd "$WD/tmp" || exit 1
+
+data="$(msm here)"
+if [[ "$data" != *"This command must be run in a Msm workspace"* ]]; then
+	echo "Using a workspace failed."
 	exit 1
 fi
+
+data="$(msm here .)"
+if [[ "$data" != *"Msm workspace set to: /"* ]]; then
+	echo "Creating a workspace failed."
+	exit 1
+fi
+
+mkdir "$WD/tmp/dir"
+cd "$WD/tmp/dir" || exit 1
+
+data="$(msm here .)"
+if [[ "$data" != *"Msm workspace set to: /"* ]]; then
+	echo "Using a nested workspace failed."
+	exit 1
+fi
+
+cd "$WD" || exit 1
+rm -rf "$WD/tmp"
 
 exit 0
 }
